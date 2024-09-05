cd53b5ef25d546a3b63061f9160ef2080441b25d
Lidia Michalska <144784986+lmichalska@users.noreply.github.com>
2024-09-05 10:44:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
+  base: '/crud-spilcafeen/', 
   plugins: [react()],
-})
+});
